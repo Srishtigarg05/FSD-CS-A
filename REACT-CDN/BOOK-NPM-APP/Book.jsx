fa0672348b925dc9b79ab69b7777f7012170c81f
Srishtigarg05 <gsrishti516@gmail.com>
2025-10-29 15:32:31 +0530

    book

---
+++ REACT-CDN/BOOK-NPM-APP/Book.jsx
@@ -0,0 +1,16 @@
+import './Book.css';
+function Book(){
+    return (
+        <div>
+            <h1><center>Book Store</center></h1>
+        <div>
+            <img src="" alt="book image" width="200" height="200" />
+            <h3>Title: Physics</h3>
+            <h4>₹Price: 385</h4>
+            <button>Add To Cart</button>
+        </div>
+        </div>
+    );
+}
+
+export default Book;
